c78e134bcbe0775934561768189acd17876148b4
zqwz <zqwz@naver.com>
2023-03-01 18:13:16 +0900

    Initial commit

---
+++ src/main/java/com/qnaBoard/QnaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.qnaBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnaBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qnaBoard/QnaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qnaBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
