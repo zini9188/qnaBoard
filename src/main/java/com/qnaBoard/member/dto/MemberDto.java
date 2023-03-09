93e8395598b63461857a78f71ead14a1afd13c8a
zqwz <zqwz@naver.com>
2023-03-09 23:14:47 +0900

    refactor(MemberDto): Member Dto 통합
    Post, Patch, Response를 하나로 통합

---
+++ src/main/java/com/qnaBoard/member/dto/MemberDto.java
@@ -0,0 +1,45 @@
+package com.qnaBoard.member.dto;
+
+import com.qnaBoard.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+public class MemberDto {
+    @Getter
+    @AllArgsConstructor
+    public static class Post{
+        @NotBlank
+        private String email;
+        @NotBlank
+        private String nickname;
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    public static class Patch{
+        private Long memberId;
+        private String nickname;
+        private String username;
+        private String password;
+        private Member.MemberStatus memberStatus;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Response{
+        private Long memberId;
+        private String email;
+        private String nickname;
+        private String username;
+        private String password;
+        private Member.MemberStatus memberStatus;
+    }
+}
