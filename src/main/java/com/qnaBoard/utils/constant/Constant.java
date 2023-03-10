45b3715a9a216392f08c5f986ee92d9b41282f22
zqwz <zqwz@naver.com>
2023-03-10 13:53:43 +0900

    refactor(Constant): 공용 사용 변수 상수화

---
+++ src/main/java/com/qnaBoard/utils/constant/Constant.java
@@ -0,0 +1,7 @@
+package com.qnaBoard.utils.constant;
+
+public class Constant {
+    public static final String DEFAULT_MEMBER_URI = "/members";
+    public static final String DEFAULT_ANSWER_URI = "/answers";
+    public static final String DEFAULT_QUESTION_URI = "/questions";
+}
