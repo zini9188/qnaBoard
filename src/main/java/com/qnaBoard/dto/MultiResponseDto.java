8b7c827f0990bf000f8e1cbfbd779633f4e9bea4
zqwz <zqwz@naver.com>
2023-03-10 14:00:32 +0900

    fix(Page): 실제 페이지보다 1 적은 문제 해결

--- src/main/java/com/qnaBoard/dto/MultiResponseDto.java
+++ src/main/java/com/qnaBoard/dto/MultiResponseDto.java
@@ -7,12 +7,12 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 @Getter
-public class MultiResponseDto <T>{
+public class MultiResponseDto<T> {
     private List<T> data;
     private PageInfo pageInfo;
 
     public MultiResponseDto(List<T> data, Page page) {
         this.data = data;
-        this.pageInfo = new PageInfo(page.getNumber(), page.getSize(), page.getTotalElements(), page.getTotalPages());
+        this.pageInfo = new PageInfo(page.getNumber() + 1, page.getSize(), page.getTotalElements(), page.getTotalPages());
     }
 }
