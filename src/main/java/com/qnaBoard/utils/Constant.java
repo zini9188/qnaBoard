cdf3aa8bb3fc6ce51522cd493ed0675c3a04514d
zqwz <zqwz@naver.com>
2023-04-28 08:40:38 +0900

    refactor: 어드민 이메일 삭제

--- src/main/java/com/qnaBoard/utils/Constant.java
+++ src/main/java/com/qnaBoard/utils/Constant.java
@@ -5,5 +5,4 @@ public class Constant {
     public static final String DEFAULT_ANSWER_URI = "/answers";
     public static final String DEFAULT_QUESTION_URI = "/questions";
     public static final String DEFAULT_LIKE_URI = "/likes";
-    public static final String DEFAULT_ADMIN_EMAIL = "admin@gmail.com";
 }
